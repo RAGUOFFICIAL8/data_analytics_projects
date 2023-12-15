969314200bebd6a2702bb6133f26400ee5acc523
RAGURAM M <153919066+RAGUOFFICIAL8@users.noreply.github.com>
2023-12-15 18:56:41 +0530

    ipl_sql_project.sql

--- ipl_sql_project.sql
+++ ipl_sql_project.sql
@@ -45,7 +45,7 @@
 ); */
 
 --question NO1: WHAT ARE THE TOP 5 PLAYERS WITH THE MOST PLAYER OF THE MATCH AWARDS?
-
+--data have been imported using import function to both the tables deliveries and matches
 select player_of_match,count(*) as awards_count
 from matches group by player_of_match
 order by awards_count desc 
